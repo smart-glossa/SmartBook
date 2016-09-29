32a43dd2a724d6e66b266a65282d912f949b73aa
SmartReviewer <reviewer@smartglossa.com>
2016-09-30 00:11:22 +0530

    Update adminValidation.js

--- WebContent/js/adminValidation.js
+++ WebContent/js/adminValidation.js
@@ -1,4 +1,13 @@
+// Try to use === always. In avoidable case use ==
+
 function validateFunction(){
+	
+ 	// var getElemValue = function(id) {
+	// 	return document.getElementById(id).value
+	// }
+	// var Name = getElemValue("Name");
+	// var dob = getElemValue("dob");
+	
 	var Name = document.getElementById("Name").value;
 	var dob = document.getElementById("dob").value;
 	var userName = document.getElementById("userName").value;
@@ -8,34 +17,47 @@ function validateFunction(){
 	// Not sure what exactly are we trying to say here
 	// If we are about to check whether the Name is empty
 	// Trim the content and check
+	
+	// if (Name.trim() === "") {
+	//  alert("Name cannot be empty");
+	// }
 	if((Name=="")||(/^\s*$/.test(Name))){
 		alert("Name can't be blank");
 	}
 	else if(Name.match(/^[a-zA-Z]+$/)){
 		// Never leave a empty block
 	}
-	else{alert("Use characters only");}
-	if((dob=="")||(/^\s*$/.test(dob))){
-	alert("Date of Birth can't be blank");
-}
-else {}
-	if((userName=="")||(/^\s*$/.test(userName))){
-	alert("User Name can't blank");
-}
-else if(userName.match(/^[a-zA-Z0-9]*$/)){
-
+	else{
+		alert("Use characters only");  // alphabets or characters , basically everything is a character
 	}
-	else{alert("Enter User Name here");}
-	if((password=="")||(/^\s*$/.test(password))){
-	alert("Enter password here");
-}
-else if(password.length<6){
-alert("minimum six characters")
-}
-else if(password.match(/^[a-zA-Z0-9]+$/)){
-
-	}else{}
 	
+	// if (dob.trim() === "") {
+	//  alert("dob cannot be empty");
+	// }
+	if((dob=="")||(/^\s*$/.test(dob))){
+	  alert("Date of Birth can't be blank");
+        }
+        else {
+	}
+	if((userName=="")||(/^\s*$/.test(userName))){
+	   alert("User Name can't blank");
+        }
+	else if(userName.match(/^[a-zA-Z0-9]*$/)){
+		// no empty block
+	}
+	else{
+		alert("Enter User Name here");
+	}
+	
+	if((password=="")||(/^\s*$/.test(password))){
+		alert("Enter password here");
+	}
+	else if(password.length<6){
+		alert("minimum six characters")
+	}
+	else if(password.match(/^[a-zA-Z0-9]+$/)){
+		// NO empty block
+	}else{}
 
 }
 
