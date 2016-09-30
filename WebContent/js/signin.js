95b0c16ca0bab8166d5cb87dadc7c7c6d75ce40b
venkatachalamd <venkat@smartglossa.com>
2016-09-30 14:22:36 +0530

    signin

--- WebContent/js/signin.js
+++ WebContent/js/signin.js
@@ -5,38 +5,31 @@ $(window).on('load',function() {
     var pw = $('#pass').val();
     
 
-    // construct URL for Add Visitors
+    
 
     var url = "http://localhost:8080/smartBook/user?operation=7&userName="+un+"&pass="+pw;
     // Call this URL for Server side actions
-//hello venkat
+
     $.ajax(url)
     .done(function(result) {
       var response = JSON.parse(result);
         // converted to JSON Object
-        if(response.type==admin){
+        if(response.type=="admin"){
           
             window.location("nav.html");
           }
-          else if(user==response.type){
+          else if(response.type=="user"){
             window.location("employeeadd.html");
           }
           else{
             alert(response);
           }
-
-        
-      /*if (response.status == 1) {
-        alert ("Successfully Added");
-      } else if (response.status == 0) {
-        alert ("Error in Form: " + response.message);
-      } else {
-        alert("Internal Error Occurs");
-      }*/
+       
+ 
     })
     .fail(function(result) {
       console.log(result);
-        //alert("Error: " + result);
+        
     });
 
   });
