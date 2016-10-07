e83d09c7748db0010e4ce6f81a25bea18f742bfc
Sathish <sathishprs@smartglossa>
2016-10-07 09:53:39 +0530

    count Class File

---
+++ src/Home/HomeDB.java
@@ -0,0 +1,33 @@
+package Home;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import common.MySqlConstant;
+
+public class HomeDB {
+	public static HomeDB getObject() {
+        return new HomeDB();
+    }
+
+	public List<HomeVO> getCountAll() throws ClassNotFoundException, SQLException {
+        List<HomeVO> members = new ArrayList<HomeVO>();
+        Statement statement = MySqlConstant.getInstance().getCreatedStatement();
+        String query = "SELECT(SELECT count(userId) FROM user ) AS user,(SELECT COUNT(projectId) FROM project) AS project,"
+        		+ "(SELECT count(issueId) FROM projectissue) AS projectissue";
+        ResultSet rs = statement.executeQuery(query);
+        if(rs.next()) {
+            HomeVO project = new HomeVO();
+            project.setuserId(rs.getInt(1));
+            project.setprojectId(rs.getInt(2));
+            project.setissueId(rs.getInt(3));
+           
+            members.add(project);
+        }
+        return members;
+    }
+
+}
