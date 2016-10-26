dc1225ddb535af326ca64f4364f6c2003bc8b8ef
venkat@smarglossa.com <priyaanbu1>
2016-10-26 15:07:38 +0530

    changes in js

--- WebContent/js/getall.js
+++ WebContent/js/getall.js
@@ -1,25 +1,26 @@
-$(window).on('load',function(){
-$('#des').click(function(){
-	var url="/smartBook/user?operation=4" ;
-$.ajax(url)
-.done(function(result) {
-	  var res=JSON.parse(result);
-  var count = res.length;
-var table=$('#div1').children();
-  var tablebody=table.remove();
-  $('#div1').append("<table  class='table-fill'></table>");
-  var table=$('#div1').children();        
- 		 		 	
- 	table.append("<tr><th  class='text-left'>userName</th><th  class='text-left'>type</th><th  class='text-left'>userId</th><th  class='text-left'>Name</th></tr>");
- 	for(var i=0;i<count;i++){
-		 var jsonObj =res[i];
- 		 var rowString = "<tr><td  class='text-left'>" + jsonObj.userName  +  "</td><td  class='text-left'>"  + jsonObj.type+ "</td><td  class='text-left'>" + jsonObj. userId+ "</td><td  class='text-left'>" + jsonObj.Name +"</td></tr>";
- 	table.append(rowString);
- }
-//alert("Internal error occurs");
-//}
-});
+$(window).on('load', function() {
+   //$('#des').click(function() {
+        var url = "/smartBook/user?operation=4";
+        $.ajax(url)
+            .done(function(result) {
+                console.log(result);
+                var res = JSON.parse(result);
+                var count = res.length;
+                var table = $('#div1').children();
+                var tablebody = table.remove();
+                $('#div1').append("<table  class='table-fill'></table>");
+                var table = $('#div1').children();
 
-});
-   
+                table.append("<tr><th  class='text-left'>Name</th><th  class='text-left'>User Name</th></th><th  class='text-left'>Type</th></tr>");
+                for (var i = 0; i < count; i++) {
+                    var jsonObj = res[i];
+                    var rowString = "<tr><td  class='text-left'>" + jsonObj.Name + "</td><td  class='text-left'>" + jsonObj.userName +  "</td><td  class='text-left'>" + jsonObj.type + "</td></tr>";
+                    table.append(rowString);
+                }
+                //alert("Internal error occurs");
+                //}
+          //  });
+
+    });
+
 });
--- WebContent/js/getallproject.js
+++ WebContent/js/getallproject.js
@@ -1,55 +1,56 @@
+
 //get all project list
-$(window).on('load',function(){
-	$('#id').click(function() {
-	var url="/smartBook/project?operation=4" ;
+$(window).on('load', function() {
+    //$('#id').click(function() {
+            var url = "/smartBook/project?operation=4";
 
-	$.ajax(url)
-.done(function(result) {
-	  var res=JSON.parse(result);
-  var count = res.length;
-var table=$('#div1').children();
-  var tablebody=table.remove();
-  $('#div1').append("<table class='tablerow'></table>");
-  var table=$('#div1').children();        
-  table.append("<tr><th>Duedate</th><th>title</th><th>SerialNo</th><th>desc</th><th>status</th><th>Action</th></tr>");
-  
-  for(var i=0;i<count;i++){
-     var jsonObj =res[i];
-     var rowString = "<tr><td>"  + jsonObj.pdate + "</td><td>"  + jsonObj.ptitle + "</td><td>" + jsonObj.projectId  +  "</td><td>" + jsonObj.dis + "</td><td>" + jsonObj.status + "</td><td>" +   "</td></tr>";
- table.append(rowString);
-     
-   // rowString.append(im);
+            $.ajax(url)
+                .done(function(result) {
+                    var res = JSON.parse(result);
+                    var count = res.length;
+                    var table = $('#div1').children();
+                    var tablebody = table.remove();
+                    $('#div1').append("<table class='table-fill'></table>");
+                    var table = $('#div1').children();
+                    table.append("<tr><th class='text-left'>SERIAL NUMBER</th><th  class='text-left'>TITLE</th><th  class='text-left'>DESCRIPTION</th><th  class='text-left'>DUEDATE</th><th  class='text-left'>STATUS</th><th  class='text-left'>ACTION</th></tr>");
 
- }
- var im=$("table td:nth-child(6)").append('<img  src="../images/4.png" id="projectId" class="remov"/>');
- //$(this).attr('id',   this.id + '_' + projectId);
- $("table td:nth-child(6)").append('<h5 id=""><a href="update.html">Update</a></h5>');
+                    for (var i = 0; i < count; i++) {
+                        var jsonObj = res[i];
+                        var rowString = "<tr><td class='text-left'>" + jsonObj.projectId + "</td><td  class='text-left'>"  + jsonObj.ptitle + "</td><td class='text-left'>" + jsonObj.dis + "</td><td  class='text-left'>" + jsonObj.pdate + "</td><td  class='text-left'>" + jsonObj.status + "</td><td>" + "</td></tr>";
+                        table.append(rowString);
 
-})
+                        // rowString.append(im);
 
-	.fail(function(result){
-    alert("error");
+                    }
+                    var im = $("table td:nth-child(6)").append('<img  src="../images/4.png" id="projectId" class="remov"/>');
+                    //$(this).attr('id',   this.id + '_' + projectId);
+                    $("table td:nth-child(6)").append('<h5 id=""><a href="update.html">Update</a></h5>');
+
+                })
+
+            .fail(function(result) {
+                alert(result);
+            });
+        //})
+        //delete the project
+    $('#projectId').click(function() {
+
+
+        //jsonObj[i].projectId==rem;
+        //arr=removejsonObj(arr,["projectId"]);
+        id = $(this).parent().attr("#projectId");
+        //remove("projectId");
+        //('projectId').remove();
+        //$(this).attr('projectId');
+        var url = "http://localhost:8080/ajith/project?mode=3&projectId=1";
+        $.ajax(url)
+            .done(function(result) {
+                //console.log(result);
+                $(this).parent().remove();
+            })
+
+        .fail(function(result) {
+            console.log(result);
+        })
     });
-    })
-//delete the project
-$('#projectId').click(function() {
-
-	
-	//jsonObj[i].projectId==rem;
-//arr=removejsonObj(arr,["projectId"]);
-	id=$(this).parent().attr("#projectId");
-//remove("projectId");
-	//('projectId').remove();
-	//$(this).attr('projectId');
-	var url="/smartBook/project?mode=3&projectId=1";
-	$.ajax(url)
-	.done(function(result){
-		//console.log(result);
-		$(this).parent().remove();
-	})
-
-	.fail(function(result){
-		console.log(result);
-	})
 });
-  });
